aa41f64414c2c16eb5502882fda508f30b586066
Duy247 <duy5a247@gmail.com>
2023-03-30 15:30:10 +0300

    Initial commit

---
+++ PkmAutoName/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace PkmAutoName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MadeByLinFang = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MadeByLinFang
+            // 
+            this.MadeByLinFang.AutoSize = true;
+            this.MadeByLinFang.Location = new System.Drawing.Point(607, 138);
+            this.MadeByLinFang.Name = "MadeByLinFang";
+            this.MadeByLinFang.Size = new System.Drawing.Size(181, 25);
+            this.MadeByLinFang.TabIndex = 0;
+            this.MadeByLinFang.Text = "Made By LinFang";
+            this.MadeByLinFang.Click += new System.EventHandler(this.MadeByLinFang_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(753, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Automated PKM naming for Pokecord. You just need to copylink and that\'s it, paste" +
+    " using Ctrl+V for the name of the pokemon";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(264, 79);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(250, 31);
+            this.lblResult.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Go to Pokedex";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = global::PkmAutoName.Properties.Resources.Derp;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 232);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(757, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Evolution info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 298);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MadeByLinFang);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PKMAutoName";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MadeByLinFang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox lblResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PkmAutoName/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PkmAutoName
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = PkmAutoName.Properties.Resources.Derp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+ 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                string url = Clipboard.GetText();
+                if (url.Contains("/regular/") && url.Contains(".png"))
+                {
+                    int comma_pos = url.LastIndexOf("/regular/") + "/regular/".Length;
+                    int dot_pos = url.LastIndexOf(".png");
+                    string item_number_str = url.Substring(comma_pos, dot_pos - comma_pos);
+                    int item_number = int.Parse(item_number_str);
+
+                    string[] lines = PkmAutoName.Properties.Resources.PKM.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                    if (item_number >= 1 && item_number <= lines.Length)
+                    {
+                        string line = lines[item_number - 1];
+                        int comma_pos2 = line.IndexOf(",");
+                        if (comma_pos2 > 0)
+                        {
+                            string item_name = line.Substring(comma_pos2 + 1);
+                            lblResult.Text =  item_name;
+                            Clipboard.SetText(item_name);
+                            string[] csvLines = PkmAutoName.Properties.Resources.Evolutions.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                            string item_evo = Array.Find(csvLines, l => l.Contains(item_name));
+                            if (item_evo != null)
+                            {
+                                    textBox1.Text = item_evo;
+                            }
+                            else
+                            {
+                                textBox1.Text = "Item name not found in CSV file.";
+                            }
+                        }
+                        else
+                        {
+                            lblResult.Text = "Invalid item format in items.txt";
+                        }
+                    }
+                    else
+                    {
+                        lblResult.Text = "Invalid item number: " + item_number;
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string item_name = lblResult.Text;
+            item_name = item_name.Replace(' ', '-');
+            string webpageUrl = "https://pokemondb.net/pokedex/" + item_name;
+            System.Diagnostics.Process.Start(webpageUrl);
+        }
+        private void MadeByLinFang_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
